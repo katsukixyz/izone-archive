5cc1e08c0899f2c598741ce5ff8fd9032716b351
Katsuki <katsukidotxyz@gmail.com>
2021-08-28 10:27:09 -0400

    chore: added page title to prefs

--- client/pages/pref.tsx
+++ client/pages/pref.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import Head from "next/head";
 import AutoplayContext from "../contexts/AutoplayContext";
 import {
   Box,
@@ -15,32 +16,37 @@ const Pref: React.FC = () => {
   const { autoplay, toggleAutoplay } = useContext(AutoplayContext);
 
   return (
-    <Center p="6">
-      <Box
-        p="6"
-        w="1000px"
-        borderRadius="6"
-        boxShadow="0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)"
-      >
-        <Heading>User preferences</Heading>
-        <Stack direction="column" mt="3">
-          <Flex direction="row" justify="space-between" align="center">
-            <Box d="flex" alignItems="baseline">
-              <Tag size="lg">Autoplay</Tag>
-              <Text display={{ base: "none", md: "inherit" }} ml="2">
-                Play videos automatically on page load
-              </Text>
-            </Box>
-            <Switch
-              isChecked={autoplay}
-              onChange={toggleAutoplay}
-              size="md"
-              colorScheme="brand"
-            />
-          </Flex>
-        </Stack>
-      </Box>
-    </Center>
+    <Box>
+      <Head>
+        <title>Preferences</title>
+      </Head>
+      <Center p="6">
+        <Box
+          p="6"
+          w="1000px"
+          borderRadius="6"
+          boxShadow="0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)"
+        >
+          <Heading>User preferences</Heading>
+          <Stack direction="column" mt="3">
+            <Flex direction="row" justify="space-between" align="center">
+              <Box d="flex" alignItems="baseline">
+                <Tag size="lg">Autoplay</Tag>
+                <Text display={{ base: "none", md: "inherit" }} ml="2">
+                  Play videos automatically on page load
+                </Text>
+              </Box>
+              <Switch
+                isChecked={autoplay}
+                onChange={toggleAutoplay}
+                size="md"
+                colorScheme="brand"
+              />
+            </Flex>
+          </Stack>
+        </Box>
+      </Center>
+    </Box>
   );
 };
 
