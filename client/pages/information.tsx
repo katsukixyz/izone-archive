43242cbccfd4502bf2bdc116401576b7aee31324
Katsuki Chan <katsukidotxyz@gmail.com>
2022-01-02 11:29:52 -0600

    fix: information width

--- client/pages/information.tsx
+++ client/pages/information.tsx
@@ -14,6 +14,8 @@ import {
   AccordionPanel,
   AccordionIcon,
   Icon,
+  Stack,
+  Flex,
 } from "@chakra-ui/react";
 import { useTranslation } from "next-i18next";
 import { BsGithub } from "react-icons/bs";
@@ -21,123 +23,94 @@ import { BsGithub } from "react-icons/bs";
 const Information: React.FC = () => {
   const { t } = useTranslation("information");
   return (
-    <Box p="4">
+    <Box>
       <Head>
         <title>Information</title>
         <meta property="og:site_name" content="IZ*ONE VLIVE Archive" />
       </Head>
-      <Center flexDir="column">
-        <Box
+      <Center p={6}>
+        <Flex
           p="6"
-          maxW="1000"
+          flexDir="column"
+          w="1000px"
           borderRadius="6"
           boxShadow="0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)"
         >
           <Heading mb="2">{t("heading")}</Heading>
-          <Text fontSize="2xl" fontWeight="semibold" mb="2">
-            {t("faq.heading")}
-          </Text>
-          <Box mb="2">
-            <Accordion allowToggle allowMultiple>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                      {t("faq.1.q")}
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel
-                  pb={3}
-                  ml={2}
-                  borderColor="gray.300"
-                  borderLeftWidth={2}
-                >
-                  {t("faq.1.a")}
-                </AccordionPanel>
-              </AccordionItem>
-
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                      {t("faq.2.q")}
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel
-                  pb={3}
-                  ml={2}
-                  borderColor="gray.300"
-                  borderLeftWidth={2}
-                >
-                  {t("faq.2.a")}
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-          </Box>
-
-          <Text fontSize="2xl" fontWeight="semibold" mb="2">
-            {t("issues.heading")}
-          </Text>
-          <Box mb="2">
-            <Text>
-              {t("issues.description")}
-              {/* Create a new issue on the{" "}
-              <Link
-                href="https://github.com/katsukixyz/izone-archive/issues"
-                isExternal
-                _hover={{ textDecoration: "none" }}
-              >
-                <Tag _hover={{ color: "brand.500", transition: "0.3s" }}>
-                  GitHub repository
-                </Tag>
-              </Link>{" "}
-              with the relevant details and reproduction methods and it will be
-              dealt with promptly. */}
+          <Stack direction="column">
+            <Text fontSize="2xl" fontWeight="semibold" mb="2">
+              {t("faq.heading")}
             </Text>
-          </Box>
-          <Text fontSize="2xl" fontWeight="semibold">
-            {t("misc.heading")}
-          </Text>
-          <Box>
-            <Text>
-              {t("misc.description")}
-              {/* For any miscellaneous questions, either leave an issue on{" "}
-              <Link
-                href="https://github.com/katsukixyz/izone-archive/issues"
-                isExternal
-                _hover={{ textDecoration: "none" }}
-              >
-                <Tag _hover={{ color: "brand.500", transition: "0.3s" }}>
-                  GitHub
-                </Tag>
-              </Link>{" "}
-              or email me at{" "}
-              <Link
-                href="mailto:katsukidotxyz@gmail.com"
-                isExternal
-                _hover={{ textDecoration: "none" }}
-              >
-                <Tag _hover={{ color: "brand.500", transition: "0.3s" }}>
-                  katsukidotxyz@gmail.com
-                </Tag>
-              </Link>
-              . */}
+            <Box mb="2">
+              <Accordion allowToggle allowMultiple>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box flex="1" textAlign="left">
+                        {t("faq.1.q")}
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel
+                    pb={3}
+                    ml={2}
+                    borderColor="gray.300"
+                    borderLeftWidth={2}
+                  >
+                    {t("faq.1.a")}
+                  </AccordionPanel>
+                </AccordionItem>
+
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box flex="1" textAlign="left">
+                        {t("faq.2.q")}
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel
+                    pb={3}
+                    ml={2}
+                    borderColor="gray.300"
+                    borderLeftWidth={2}
+                  >
+                    {t("faq.2.a")}
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </Box>
+
+            <Text fontSize="2xl" fontWeight="semibold" mb="2">
+              {t("issues.heading")}
             </Text>
-          </Box>
+            <Box mb="2">
+              <Text>{t("issues.description")}</Text>
+            </Box>
+            <Text fontSize="2xl" fontWeight="semibold">
+              {t("misc.heading")}
+            </Text>
+            <Box>
+              <Text>{t("misc.description")}</Text>
+            </Box>
+          </Stack>
           <Center mt={5}>
             <Link
               href="https://github.com/katsukixyz/izone-archive"
               target="_blank"
               rel="noopener noreferrer"
+              _hover={{
+                cursor: "pointer",
+                color: "brand.500",
+                transition: "0.3s",
+              }}
             >
-              <Icon as={BsGithub} w={6} h={6} _hover={{ cursor: "pointer" }} />
+              <Icon as={BsGithub} w={6} h={6} />
             </Link>
           </Center>
-        </Box>
+        </Flex>
       </Center>
     </Box>
   );
